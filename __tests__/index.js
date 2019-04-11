bb224433c4f5215e0a3f8aa7cfae1285094b8499
Andre Galdino <andre.galdinolima@gmail.com>
2019-04-11 16:08:42 -0300

    cover when input is 15, must return FizzBuzz

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,14 +1,20 @@
 import FizzBuzz from '../src'
 describe('fizzBuzz', () => {
-    test('when input is 3 must return fizz', () => {
-        const fizz = new FizzBuzz()
-        const result = fizz.run(3)
-        expect(result).toBe('Fizz')
-    })
+  test('when input is 3 must return fizz', () => {
+    const fizz = new FizzBuzz()
+    const result = fizz.run(3)
+    expect(result).toBe('Fizz')
+  })
 
-    test('when input is 5 must return Buzz', () => {
-      const fizz = new FizzBuzz()
-      const result = fizz.run(5)
-      expect(result).toBe('Buzz')
+  test('when input is 5 must return Buzz', () => {
+    const fizz = new FizzBuzz()
+    const result = fizz.run(5)
+    expect(result).toBe('Buzz')
+  })
+
+  test('when input is 15 must return FizzBuzz', () => {
+    const fizz = new FizzBuzz()
+    const result = fizz.run(15)
+    expect(result).toBe('FizzBuzz')
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 export default class FizzBuzz {
   run(number) {
+
+    if (((number % 3) === 0) && ((number % 5) === 0)) {
+      return 'FizzBuzz'
+    }
+
     if ((number % 3) === 0) {
       return 'Fizz'
     }
